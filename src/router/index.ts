f7ff4fc2c2d9ecf85933075fe28b9a074a50ffef
Ali <alimanashov@gmail.com>
2023-09-20 21:58:52 +0600

    Vercel deploy updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,17 +5,17 @@ const router = createRouter({
   routes: [
     {
       name: "home",
-      component: import("@/views/HomeView.vue"),
-      path: "",
+      component: () => import("@/views/HomeView.vue"),
+      path: "/",
     },
     {
       name: "picture-page",
-      component: import("@/views/PictureView.vue"),
+      component: () => import("@/views/PictureView.vue"),
       path: "/pictures/:pictureId"
     },
     {
       name: "favorites",
-      component: import("@/views/FavoritesView.vue"),
+      component: () => import("@/views/FavoritesView.vue"),
       path: "/favorites"
     },
   ]
